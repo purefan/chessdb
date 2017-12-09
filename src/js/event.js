443397f9ebee2bed27aff1c95a320b4678e451d5
Miguel Espinoza <wiseit@outlook.com>
2017-12-09 12:41:48 +0100

    Embryo for event handling based on slots and signals

--- src/js/event.js
+++ src/js/event.js
@@ -1,4 +1,27 @@
 /**
  * Event manager for vogula. Implements a slot/signal mechanism and debugging tools
  */
-vogula.event = {}
+vogula.event = {
+    slots: {}
+}
+vogula.event.init = () => {}
+
+/**
+ * Registers a function to be called when event_name is triggered
+ * @param event_name string
+ * @param fn function
+ */
+vogula.event.listen = function listen(event_name, fn) {
+    if (!this.slots[event_name]) {
+        this.slots[event_name] = []
+    }
+    this.slots[event_name].push(fn)
+}
+
+vogula.event.trigger = function(event_name, args) {
+    vogula.log(`Triggering the event ${event_name}`)
+    if (!this.slots[event_name]) {
+        return vogula.log.error(`No one is listening for the event ${event_name}`)
+    }
+    this.slots.forEach((fn) => fn(args))
+}
