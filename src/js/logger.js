b7e78c54abd8bbe1f81146b1383d9410a52520b0
Miguel Espinoza <wiseit@outlook.com>
2017-12-09 12:42:06 +0100

    Embryo for error logging

--- src/js/logger.js
+++ src/js/logger.js
@@ -10,6 +10,18 @@ vogula.log = (what, level) => {
         .catch((e) => console.error('logger::constructor::error getting setting',e))
 }
 
+vogula.log.error = function error(what, level) {
+    vogula.setting.get('log.level')
+    .then(log_level => {
+        // if ( log_level >= (level || vogula.log.setting.default_level)) {
+            const string_level = Object.keys(vogula.log.setting.levels).find(key => vogula.log.setting.levels[key] === level) || 'NotFound'
+            const safe_what = JSON.stringify(what)
+            console.error(`[${string_level}] ${safe_what}`)
+        // }
+        })
+    .catch((e) => console.error('logger::constructor::error getting setting',e))
+}
+
 // define default values for the setting
 vogula.log.setting = {
     levels : {
