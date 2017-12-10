544d6847c2ad3fd5b0f55198f892e155dbf594f6
Miguel Espinoza <wiseit@outlook.com>
2017-12-10 01:21:54 +0100

    Better folder structure

--- tools/gulp/html.js
+++ tools/gulp/html.js
@@ -7,29 +7,43 @@ const jade = require('gulp-jade')
 // const del = require('node-delete')
 const jade_concat = require('gulp-jade-template-concat');
 const del = require('del')
-
+const debug = require('gulp-debug')
+const fs = require('fs')
 /**
- * this points to the gulp object
+ * `this` points to the gulp object
  */
 function html() {
-    const src_jade = path.join(this.settings.path.src, 'html') + path.sep + '**.jade'
-    watch(src_jade, () => {
-        console.log('Reloading templates')
+    const src_core_jade = path.join(this.settings.path.src, 'html', 'core') + path.sep + '**.jade'
+    const src_modals_jade = path.join(this.settings.path.src, 'html', 'modals') + path.sep + '**' + path.sep + '**.jade'
+
+    const watch_jade_core_files = () => {
+        console.log(`[html] Reloading templates from ${src_core_jade}`)
+        const core_files = []
         const stream = this
-            .src(src_jade)
+            .src(src_core_jade)
+            .pipe(debug({title: 'core::jade'}))
             .pipe(jade({
                 pretty: true
             }))
             // .pipe(jade_concat('index.html', {templateVariable:"templates"}))
-            .pipe(this.dest(path.join(this.settings.path.dist)))
+            .pipe(this.dest(this.settings.path.dist))
         stream.on('end', () => {
-            console.log('stream ended, deleting extra html files')
             del(
                 [path.join(this.settings.path.dist, '*.html'),
                 '!' + path.join(this.settings.path.dist,'index.html')]
             ).then( path => console.log('Deleted', path))
         })
-    })
+    }
 
+    const watch_modals = () => {
+        const stream = this
+            .src(src_modals_jade)
+            .pipe(jade({pretty: true}))
+            .pipe(this.dest(path.join(this.settings.path.dist,'modals')))
+    }
+
+    watch(src_core_jade, watch_jade_core_files)
+    watch(src_modals_jade, watch_modals)
 }
+
 module.exports = html
--- tools/gulp/js.js
+++ tools/gulp/js.js
@@ -5,18 +5,25 @@ const debug = require('gulp-debug')
 const gap = require('gulp-append-prepend')
 
 function js() {
-    const src_js = path.join(this.settings.path.src, 'js', '**') + path.sep + '**.js'
-    console.log(`[js] Watching ${src_js}`)
-    watch(src_js, () => {
-        console.log('Reloading javascript ' + (new Date()))
+    const src_core_js = path.join(this.settings.path.src, 'js', 'core') + path.sep + '**.js'
+    const src_modal = path.join(this.settings.path.src, 'js', 'modal', '**') + path.sep + '**.js'
+    console.log(`[js] Watching ${src_core_js}`)
+    console.log(`[js] Watching ${src_modal}`)
+    watch(src_core_js, () => {
+        console.log('Reloading core javascript ' + (new Date()))
         this
-            .src([src_js, '!' + path.join(this.settings.path.src, 'js', 'core', 'vogula.js')])
-            .pipe(debug())
+            .src([src_core_js, '!' + path.join(this.settings.path.src, 'js', 'core', 'vogula.js')])
+            .pipe(debug({title: 'js::core'}))
             .pipe(concat('vogula.js'))
             .pipe(gap.prependFile(path.join(this.settings.path.src, 'js', 'core', 'vogula.js')))
-
-
             .pipe(this.dest(path.join(this.settings.path.dist)))
+    })
+
+    watch(src_modal, () => {
+        this
+            .src([src_modal])
+            .pipe(debug({title: 'js::modal'}))
+            .pipe(this.dest(path.join(this.settings.path.dist, 'modals', 'js')))
     })
 }
 
