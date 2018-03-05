94d15843bedaa3f638c1db734112dd8bd55fca58
purefan <wiseit@outlook.com>
2018-03-06 00:37:22 +0100

    Clean up

--- src/Scene/Content/Main/ChessEngine/index.js
+++ src/Scene/Content/Main/ChessEngine/index.js
@@ -9,11 +9,10 @@ const engine_panel = {
         const settings = vnode.attrs.settings.all
         let engine_settings = find_default_engine(vnode.attrs.settings.all)
         if (!engine_settings) {
-            console.log(vnode)
             vnode.attrs.eventer.on('vendor.purefan.settings.init', () => {
                 engine_settings = find_default_engine(vnode.attrs.settings.all)
                 prepare_engine(vnode, engine_settings)
-        }, {once: true})
+            }, { once: true })
         } else {
             prepare_engine(vnode, engine_settings)
         }
@@ -24,22 +23,18 @@ const engine_panel = {
                 settings.available_engines &&
                 settings.available_engines.manage_engines &&
                 settings.available_engines.manage_engines.value) {
-                    return settings.available_engines.manage_engines.value[0]
-                }
+                return settings.available_engines.manage_engines.value[0]
+            }
             return false
         }
 
-        function prepare_engine (vnode, engine_settings){
+        function prepare_engine(vnode, engine_settings) {
             if (!vnode.state.engine) {
                 return
             }
             vnode.state.engine = new vnode.attrs.UCIEngine(engine_settings.path)
             vnode.state.engine.eventer = vnode.attrs.eventer
-            vnode.attrs.eventer.on('vendor.purefan.engine.info', (what) => {
-                console.log('Content::Main::ChessEngine::info', what)
-                self.engine_output = what
-                setTimeout(vnode.attrs.m.redraw)
-            })
+
             setTimeout(vnode.attrs.m.redraw)
 
             vnode.attrs.eventer.on('libase.board.changed', (move) => {
@@ -61,13 +56,18 @@ const engine_panel = {
         const display_moves = require('./linesDisplayer')
 
         const btn_configure = vnode.attrs.m('button', 'configure')
-        const toolbar       = vnode.attrs.m('div', {class: 'toolbar'},
+        const toolbar = vnode.attrs.m('div', { class: 'toolbar' },
             [
                 btn_toggle_analysis
                 , btn_configure
-        ]
+            ]
         )
-        return vnode.attrs.m('div', {class: 'chess-engine'}, toolbar, vnode.attrs.m(display_moves, vnode.attrs))
+        return vnode.attrs.m(
+            'div',
+            { class: 'chess-engine' },
+            toolbar,
+            vnode.attrs.m(display_moves, vnode.attrs)
+        )
     }
 }
 
